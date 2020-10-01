5d4e4699f5b7b2c6d77d2c9b3a6ef252fe7badd4
Ammar <ar.nassri@gmail.com>
2020-10-01 03:20:53 -0400

    Campaign browser, details pages, and some material UI implemented

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -6,10 +6,10 @@ function Navigation(props) {
     return (
         <nav>
             <ul>
-                <li><NavLink to="/" activeClass="active">Home</NavLink></li>
-                <li><NavLink to="/users" activeClass="active">Users</NavLink></li>
+                <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
+                <li><NavLink to="/users" activeClassName="active">Users</NavLink></li>
                 <li>{props.needLogin 
-                 ? <NavLink to="/login" activeClass="active">Login</NavLink> 
+                    ? <NavLink to="/login" activeClassName="active">Login</NavLink> 
                  : <LogoutButton/>} </li>
                 
             </ul>
