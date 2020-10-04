f408a1c7bdf6f0a47e18a643858c02941d8a3ee0
Ammar Nassri <anassri@Ammars-MBP.attlocal.net>
2020-10-03 22:13:50 -0400

    login, sign up validations implemented, no issues noted

--- aidies-project/client/src/components/LoginPanel.js
+++ aidies-project/client/src/components/LoginPanel.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { login } from '../store/authentication';
+import Alert from '@material-ui/lab/Alert';
 
 import {
     Button,
@@ -43,6 +44,7 @@ const LoginPanel = (props) => {
     const [password, setPassword] = useState('password');
     const dispatch = useDispatch();
     const userId = useSelector(state => state.authentication.user.id);
+    const errors = useSelector(state => state.authentication.errors);
     const location = useSelector(state => state.campaign.previousLocation);
     const classes = useStyles();
 
@@ -58,59 +60,62 @@ const LoginPanel = (props) => {
     }
     return (
         <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={classes.paper}>
+            <CssBaseline />
+            <div className={classes.paper}>
             
-            <Typography component="h1" variant="h5">
-                        Login
-            </Typography>
+                <Typography component="h1" variant="h5">
+                    Login
+                </Typography>
                 <form className={classes.form} noValidate onSubmit={handleSubmit}>
-            <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-            />
-            <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                className={classes.submit}
-            >
-                            Login
-            </Button>
-            <Grid container>
-                <Grid item>
-                    <Link href='/signup' variant="body2" className={classes.link}>
-                        {"Don't have an account? Sign Up"}
-                    </Link>
-                </Grid>
-            </Grid>
-            </form>
-        </div>
-    </Container>
-        
-        
+                    {errors.length
+                        ? <Alert variant="outlined" severity="error" >
+                            {errors.map((error, i) => <li key={i} className="error-list-item">{error}</li>)}
+                        </Alert>
+                        : null}
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        className={classes.submit}
+                    >
+                        Login
+                    </Button>
+                    <Grid container>
+                        <Grid item>
+                            <Link href='/signup' variant="body2" className={classes.link}>
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+        </Container>
     )
 }
 
